c471d743ba17ae1ca27e90d7f661ddd2af440a86
raquels29 <raquel.bento.santos@tecnico.ulisboa.pt>
2020-11-03 00:10:31 +0000

    initial files

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p compiled images
+
+for i in sources/*.txt tests/*.txt; do
+	echo "Compiling: $i"
+    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
+done
+
+
+# TODO
+
+
+for i in compiled/*.fst; do
+	echo "Creating image: images/$(basename $i '.fst').pdf"
+    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
+done
+
+echo "Testing the transducer 'converter' with the input 'tests/numero.txt'"
+fstcompose compiled/numero.fst compiled/converter.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
